84bc93bea5f95a893d344dd55c3093ca0661a599
서주희 <maywngml@naver.com>
2025-03-31 02:10:26 +0900

    [feat] eventManager 함수 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,32 @@
-export function setupEventListeners(root) {}
+const eventMap = {};
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  Object.entries(eventMap).forEach(([eventType, events], index) => {
+    eventMap[eventType][index].root = root;
+    root.addEventListener(eventType, (e) => {
+      events.forEach(({ element, handler }) => {
+        if (e.target === element) {
+          handler();
+        }
+      });
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  eventMap[eventType] = eventMap[eventType] ?? [];
+  eventMap[eventType].push({ element, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  const index = eventMap[eventType].findIndex(
+    ({ element: currentElement, handler: currentHandler }) =>
+      currentElement.isSameNode(element) &&
+      currentHandler.toString() === handler.toString(),
+  );
+
+  if (index >= 0) {
+    eventMap[eventType][index].root.removeEventListener(eventType, handler);
+    eventMap[eventType].splice(index, 1);
+  }
+}
