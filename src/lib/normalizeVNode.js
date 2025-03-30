e4b6d8e4bc575543662cf8ad4005db13d0e87b1f
서주희 <maywngml@naver.com>
2025-03-31 00:24:43 +0900

    [feat] normalizeVNode 함수 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (typeof vNode === "boolean" || vNode === undefined || vNode === null) {
+    return "";
+  } else if (typeof vNode === "string") {
+    return vNode;
+  } else if (typeof vNode === "number") {
+    return vNode.toString();
+  } else if (typeof vNode.type === "function") {
+    const node = vNode.type({ ...vNode.props, children: vNode.children });
+    return normalizeVNode(node);
+  } else {
+    return {
+      ...vNode,
+      children: vNode.children
+        .flatMap(normalizeVNode)
+        .filter((item) => Boolean(item)),
+    };
+  }
 }
