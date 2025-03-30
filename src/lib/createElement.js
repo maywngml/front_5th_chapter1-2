4054d8ad20cb975decafbda33574cc0cf918a817
서주희 <maywngml@naver.com>
2025-03-31 01:25:05 +0900

    [feat] createElement 함수 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,31 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (typeof vNode === "boolean" || vNode === undefined || vNode === null) {
+    return document.createTextNode("");
+  } else if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  } else if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...vNode.map(createElement));
+    return fragment;
+  } else if (typeof vNode.type === "function") {
+    throw Error;
+  } else {
+    const element = document.createElement(vNode.type);
+    if (vNode.props) {
+      updateAttributes(element, vNode.props);
+    }
+    vNode.children.forEach((item) => {
+      element.appendChild(createElement(item));
+    });
+    return element;
+  }
+}
 
-function updateAttributes($el, props) {}
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    const translatedKey = key === "className" ? "class" : key;
+    $el.setAttribute(translatedKey, value);
+  });
+}
